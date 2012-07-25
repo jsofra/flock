651b85c166df817ddea67665980845947b376abc
James Sofra <James.Sofra@gmail.com>
2012-07-25 16:36:42 +1000

    add :gen-class

--- src/flock/core.clj
+++ src/flock/core.clj
@@ -1,14 +1,14 @@
 (ns flock.core
   (:use quil.core)
-  (:require [flock.dynamic :as dyn]))
-
-(defsketch flock-sketch
-  :title "Flock!"
-  :setup dyn/setup
-  :draw dyn/draw
-  :mouse-released dyn/mouse-released
-  :mouse-moved dyn/mouse-moved
-  :size [800 800])
+  (:require [flock.dynamic :as dyn])
+  (:gen-class))
 
 (defn -main [& args]
-  (println "Start Flocking!"))
+  (println "Start Flocking!")
+  (defsketch flock-sketch
+    :title "Flock!"
+    :setup dyn/setup
+    :draw dyn/draw
+    :mouse-released dyn/mouse-released
+    :mouse-moved dyn/mouse-moved
+    :size [800 800]))
